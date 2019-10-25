pmatalon/fhhos4

commit cfab9aada6124d2c534dc485cf4b0710c384d501

CartesianFace implementing HHO_Face and DG_Face

src/DG/Poisson_DG_Face.h
@@ -8,6 +8,65 @@ class Poisson_DG_Face : virtual public Face<Dim>
 public:
 	Poisson_DG_Face(BigNumber number, Element<Dim>* element1, Element<Dim>* element2) : Face<Dim>(number, element1, element2) {}
 
-	virtual double CouplingTerm(Element<Dim>* element1, BasisFunction<Dim>* phi1, Element<Dim>* element2, BasisFunction<Dim>* phi2, DiffusionPartition diffusionPartition) = 0;
-	virtual double PenalizationTerm(Element<Dim>* element1, BasisFunction<Dim>* phi1, Element<Dim>* element2, BasisFunction<Dim>* phi2, double penalizationCoefficient, DiffusionPartition diffusionPartition) = 0;
+	virtual double CouplingTerm(Element<Dim>* element1, BasisFunction<Dim>* p_phi1, Element<Dim>* element2, BasisFunction<Dim>* p_phi2, DiffusionPartition diffusionPartition)
+	{
+		auto n1 = element1->OuterNormalVector(this);
+		auto n2 = element2->OuterNormalVector(this);
+
+		double k1 = element1->DiffusionCoefficient(diffusionPartition);
+		double k2 = element2->DiffusionCoefficient(diffusionPartition);
+
+		double weight1 = 1;
+		double weight2 = 1;
+		if (!this->IsDomainBoundary)
+		{
+			Element<Dim>* elementOnTheOtherSide1 = element1->ElementOnTheOtherSideOf(this);
+			Element<Dim>* elementOnTheOtherSide2 = element2->ElementOnTheOtherSideOf(this);
+			double l1 = k1;
+			double l2 = elementOnTheOtherSide1->DiffusionCoefficient(diffusionPartition);
+			weight1 = elementOnTheOtherSide1->DiffusionCoefficient(diffusionPartition) / (l1 + l2);
+			weight2 = elementOnTheOtherSide2->DiffusionCoefficient(diffusionPartition) / (l1 + l2);
+		}
+
+		auto phi1 = element1->EvalPhiOnFace(this, p_phi1);
+		auto gradPhi1 = element1->GradPhiOnFace(this, p_phi1);
+
+		auto phi2 = element2->EvalPhiOnFace(this, p_phi2);
+		auto gradPhi2 = element2->GradPhiOnFace(this, p_phi2);
+
+		std::function<double(RefPoint)> functionToIntegrate = [n1, n2, phi1, phi2, gradPhi1, gradPhi2, weight1, weight2, k1, k2](RefPoint p) {
+			double meanGradPhi1_scal_jumpPhi2 = weight1 * k1 * Utils::InnerProduct<Dim>(gradPhi1(p), n2) * phi2(p);
+			double meanGradPhi2_scal_jumpPhi1 = weight2 * k2 * Utils::InnerProduct<Dim>(gradPhi2(p), n1) * phi1(p);
+			return meanGradPhi1_scal_jumpPhi2 + meanGradPhi2_scal_jumpPhi1;
+		};
+
+		int polynomialDegree = p_phi1->GetDegree() + p_phi2->GetDegree() - 1;
+		double integralJump1ScalarJump2 = this->ComputeIntegral(functionToIntegrate, 1, polynomialDegree);
+	}
+
+	virtual double PenalizationTerm(Element<Dim>* element1, BasisFunction<Dim>* p_phi1, Element<Dim>* element2, BasisFunction<Dim>* p_phi2, double penalizationCoefficient, DiffusionPartition diffusionPartition)
+	{
+		auto n1 = element1->OuterNormalVector(this);
+		auto n2 = element2->OuterNormalVector(this);
+
+		auto phi1 = element1->EvalPhiOnFace(this, p_phi1);
+		auto phi2 = element2->EvalPhiOnFace(this, p_phi2);
+
+		std::function<double(RefPoint)> functionToIntegrate = [phi1, phi2, n1, n2](RefPoint p) {
+			return Utils::InnerProduct<Dim>(n1, n2) * phi1(p) * phi2(p);
+		};
+
+		int polynomialDegree = p_phi1->GetDegree() + p_phi2->GetDegree();
+		double integralJump1ScalarJump2 = this->ComputeIntegral(functionToIntegrate, 0, polynomialDegree);
+
+		double diffusionDependantCoefficient = element1->DiffusionCoefficient(diffusionPartition);
+		if (!this->IsDomainBoundary)
+		{
+			double k1 = this->Element1->DiffusionCoefficient(diffusionPartition);
+			double k2 = this->Element2->DiffusionCoefficient(diffusionPartition);
+			diffusionDependantCoefficient = 2 * k1*k2 / (k1 + k2);
+		}
+
+		return diffusionDependantCoefficient * penalizationCoefficient * integralJump1ScalarJump2;
+	}
 };

DGHHO.vcxproj
@@ -102,6 +102,7 @@
     <ClInclude Include="src/FunctionalBasis/TensorPolynomial.h" />
     <ClInclude Include="src\FunctionalBasis\Hemker1D.h" />
     <ClInclude Include="src\Mesh\CartesianElement.h" />
+    <ClInclude Include="src\Mesh\CartesianFace.h" />
     <ClInclude Include="src\Mesh\CartesianShape.h" />
     <ClInclude Include="src\Mesh\Mesh.h" />
     <ClInclude Include="src/Mesh/CartesianGrid1D.h" />

DGHHO.vcxproj.filters
@@ -149,6 +149,9 @@
     <ClInclude Include="src\Solver\IterationResult.h">
       <Filter>Solver</Filter>
     </ClInclude>
+    <ClInclude Include="src\Mesh\CartesianFace.h">
+      <Filter>Mesh</Filter>
+    </ClInclude>
   </ItemGroup>
   <ItemGroup>
     <Filter Include="Utils">

src/HHO/Poisson_HHO_Face.h
@@ -60,6 +60,32 @@ public:
 
 	virtual Eigen::MatrixXd ComputeAndReturnFaceMassMatrix(FunctionalBasis<Dim-1>* basis) = 0;
 
+	Eigen::MatrixXd MassMatrix(FunctionalBasis<Dim - 1>* basis, Element<Dim>* element, FunctionalBasis<Dim>* cellBasis)
+	{
+		Eigen::MatrixXd M(basis->LocalFunctions.size(), cellBasis->LocalFunctions.size());
+		for (BasisFunction<Dim - 1>* phi1 : basis->LocalFunctions)
+		{
+			for (BasisFunction<Dim>* phi2 : cellBasis->LocalFunctions)
+			{
+				double term = this->ComputeMassTerm(phi1, element, phi2);
+				M(phi1->LocalNumber, phi2->LocalNumber) = term;
+			}
+		}
+		return M;
+	}
+
+	double ComputeMassTerm(BasisFunction<Dim - 1>* facePhi, Element<Dim>* element, BasisFunction<Dim>* reconstructPhi)
+	{
+		auto reconstructPhiOnFace = element->EvalPhiOnFace(this, reconstructPhi);
+
+		function<double(RefPoint)> functionToIntegrate = [facePhi, reconstructPhiOnFace](RefPoint p) {
+			return facePhi->Eval(p) * reconstructPhiOnFace(p);
+		};
+
+		int polynomialDegree = facePhi->GetDegree() + reconstructPhi->GetDegree();
+		return this->ComputeIntegral(functionToIntegrate, 0, polynomialDegree);
+	}
+
 	Eigen::MatrixXd GetMassCellFace(Element<Dim>* element)
 	{
 		if (element == this->Element1)
@@ -95,13 +121,4 @@ public:
 			return _elem2_projFromCell;
 		assert(false);
 	}
-
-	Eigen::MatrixXd GetProjFromCoarserReconstruct()
-	{
-		Poisson_HHO_Element<Dim>* fineElement = dynamic_cast<Poisson_HHO_Element<Dim>*>(this->Element1);
-
-	}
-
-	virtual double ComputeIntegral(function<double(RefPoint)> func, int numberOfDerivatives) = 0;
-	virtual double ComputeIntegral(function<double(RefPoint)> func, int numberOfDerivatives, int polynomialDegree) = 0;
 };

src/Mesh/CartesianFace.h
@@ -1,0 +1,58 @@
+#pragma once
+#include "Face.h"
+#include "CartesianShape.h"
+#include "../HHO/Poisson_HHO_Face.h"
+
+template <int Dim>
+class CartesianFace : public Poisson_DG_Face<Dim>, public Poisson_HHO_Face<Dim>, public CartesianShape<Dim, Dim-1>
+{
+public:
+
+	CartesianFace(BigNumber number, DomPoint origin, double width, Element<Dim>* element1, Element<Dim>* element2, CartesianShapeOrientation orientation) :
+		Poisson_DG_Face<Dim>(number, element1, element2),
+		Poisson_HHO_Face<Dim>(number, element1, element2), 
+		CartesianShape<Dim, Dim - 1>(origin, width, orientation)
+	{}
+
+	//----------------------------------------------------//
+	//                 Face implementation                //
+	//----------------------------------------------------//
+
+	double GetDiameter()
+	{
+		return CartesianShape<Dim, Dim - 1>::Width;
+	}
+
+	double Measure()
+	{
+		return CartesianShape<Dim, Dim - 1>::Measure();
+	}
+
+	RefPoint ConvertToReference(DomPoint domainPoint)
+	{
+		return CartesianShape<Dim, Dim - 1>::ConvertToReference(domainPoint);
+	}
+	DomPoint ConvertToDomain(RefPoint referenceElementPoint)
+	{
+		return CartesianShape<Dim, Dim - 1>::ConvertToDomain(referenceElementPoint);
+	}
+
+	double ComputeIntegral(function<double(RefPoint)> func, int numberOfDerivatives)
+	{
+		return CartesianShape<Dim, Dim - 1>::ComputeIntegral(func, numberOfDerivatives);
+	}
+	double ComputeIntegral(function<double(RefPoint)> func, int numberOfDerivatives, int polynomialDegree)
+	{
+		return CartesianShape<Dim, Dim - 1>::ComputeIntegral(func, numberOfDerivatives, polynomialDegree);
+	}
+
+	//----------------------------------------------------------------//
+	//                 Poisson_HHO_Face implementation                //
+	//----------------------------------------------------------------//
+
+	Eigen::MatrixXd ComputeAndReturnFaceMassMatrix(FunctionalBasis<Dim-1>* basis)
+	{
+		return CartesianShape<Dim, Dim - 1>::ComputeAndReturnFaceMassMatrix(basis);
+	}
+
+};

src/Mesh/Face.h
@@ -11,6 +11,7 @@ public:
 	Element<Dim>* Element2;
 
 public:
+	Face() { assert(false); }
 	Face(BigNumber number, Element<Dim>* element1, Element<Dim>* element2)
 	{
 		this->Number = number;
@@ -44,29 +45,12 @@ public:
 		return NULL;
 	}
 
-	virtual double MassTerm(BasisFunction<Dim - 1>* facePhi, Element<Dim>* element, BasisFunction<Dim>* reconstructPhi) = 0;
-
 	virtual double GetDiameter() = 0;
 	virtual double Measure() = 0;
-
 	virtual DomPoint ConvertToDomain(RefPoint refPoint) = 0;
+	virtual double ComputeIntegral(function<double(RefPoint)> func, int numberOfDerivatives) = 0;
+	virtual double ComputeIntegral(function<double(RefPoint)> func, int numberOfDerivatives, int polynomialDegree) = 0;
 	
-	Eigen::MatrixXd MassMatrix(FunctionalBasis<Dim-1>* basis, Element<Dim>* element, FunctionalBasis<Dim>* cellBasis)
-	{
-		Eigen::MatrixXd M(basis->LocalFunctions.size(), cellBasis->LocalFunctions.size());
-		for (BasisFunction<Dim-1>* phi1 : basis->LocalFunctions)
-		{
-			for (BasisFunction<Dim>* phi2 : cellBasis->LocalFunctions)
-			{
-				double term = this->MassTerm(phi1, element, phi2);
-				M(phi1->LocalNumber, phi2->LocalNumber) = term;
-			}
-		}
-		return M;
-	}
-
-	virtual vector<RefPoint> GetNodalPoints(FunctionalBasis<Dim-1>* basis) = 0;
-
 	virtual string ToString()
 	{
 		return "Interface " + to_string(this->Number);

src/Mesh/IntervalFace.h
@@ -1,167 +1,26 @@
 #pragma once
 #include "Face.h"
+#include "CartesianFace.h"
 #include "../Utils/Utils.h"
 #include "../DG/Poisson_DG_Face.h"
-#include "../HHO/Poisson_HHO_Face.h"
 
-class IntervalFace : virtual public Face<2>, public CartesianShape<2,1>, public Poisson_DG_Face<2>, public Poisson_HHO_Face<2>
+class IntervalFace : public CartesianFace<2>
 {
 public:
 
 	IntervalFace(BigNumber number, DomPoint origin, double length, Element<2>* element1, Element<2>* element2, CartesianShapeOrientation orientation) : 
 		Face(number, element1, element2), 
-		CartesianShape(origin, length, orientation),
-		Poisson_DG_Face(number, element1, element2),
-		Poisson_HHO_Face(number, element1, element2)
-	{
-	}
+		CartesianFace(number, origin, length, element1, element2, orientation)
+	{ }
 
 	IntervalFace(BigNumber number, DomPoint origin, double length, Element<2>* element1, CartesianShapeOrientation orientation) :
 		Face(number, element1), 
-		CartesianShape(origin, length, orientation),
-		Poisson_DG_Face(number, element1, NULL),
-		Poisson_HHO_Face(number, element1, NULL)
-	{
-	}
-
-	//----------------------------------------------------//
-	//                 Face implementation                //
-	//----------------------------------------------------//
-
-	double GetDiameter()
-	{
-		return CartesianShape::Width;
-	}
-
-	double Measure()
-	{
-		return CartesianShape::Measure();
-	}
-
-	double MassTerm(BasisFunction<1>* facePhi, Element<2>* element, BasisFunction<2>* reconstructPhi) override
-	{
-		double h = this->Width;
-		auto reconstructPhiOnFace = element->EvalPhiOnFace(this, reconstructPhi);
-
-		function<double(double)> functionToIntegrate = [facePhi, reconstructPhiOnFace](double t) {
-			return facePhi->Eval(t) * reconstructPhiOnFace(t);
-		};
-
-		int nQuadPoints = facePhi->GetDegree() + reconstructPhi->GetDegree() + 2;
-		return h / 2 * Utils::Integral(nQuadPoints, functionToIntegrate, -1, 1);
-	}
-
-	RefPoint ConvertToReference(DomPoint domainPoint)
-	{
-		return CartesianShape::ConvertToReference(domainPoint);
-	}
-
-	DomPoint ConvertToDomain(RefPoint referenceElementPoint)
-	{
-		return CartesianShape::ConvertToDomain(referenceElementPoint);
-	}
-
-	vector<RefPoint> GetNodalPoints(FunctionalBasis<1>* basis)
-	{
-		return CartesianShape::GetNodalPoints(basis);
-	}
-
-	//---------------------------------------------------------------//
-	//                 Poisson_DG_Face implementation                //
-	//---------------------------------------------------------------//
-
-	double CouplingTerm(Element<2>* element1, BasisFunction<2>* p_phi1, Element<2>* element2, BasisFunction<2>* p_phi2, DiffusionPartition diffusionPartition)
-	{
-		auto n1 = element1->OuterNormalVector(this);
-		auto n2 = element2->OuterNormalVector(this);
-
-		double k1 = element1->DiffusionCoefficient(diffusionPartition);
-		double k2 = element2->DiffusionCoefficient(diffusionPartition);
-
-		double weight1 = 1;
-		double weight2 = 1;
-		if (!this->IsDomainBoundary)
-		{
-			Element<2>* elementOnTheOtherSide1 = element1->ElementOnTheOtherSideOf(this);
-			Element<2>* elementOnTheOtherSide2 = element2->ElementOnTheOtherSideOf(this);
-			double l1 = k1;
-			double l2 = elementOnTheOtherSide1->DiffusionCoefficient(diffusionPartition);
-			weight1 = elementOnTheOtherSide1->DiffusionCoefficient(diffusionPartition) / (l1 + l2);
-			weight2 = elementOnTheOtherSide2->DiffusionCoefficient(diffusionPartition) / (l1 + l2);
-		}
-
-		auto phi1 = element1->EvalPhiOnFace(this, p_phi1);
-		auto gradPhi1 = element1->GradPhiOnFace(this, p_phi1);
-
-		auto phi2 = element2->EvalPhiOnFace(this, p_phi2);
-		auto gradPhi2 = element2->GradPhiOnFace(this, p_phi2);
-
-		std::function<double(double)> functionToIntegrate = [n1, n2, phi1, phi2, gradPhi1, gradPhi2, weight1, weight2, k1, k2](double u) {
-			RefPoint p(u);
-			double meanGradPhi1_scal_jumpPhi2 = weight1 * k1 * InnerProduct(gradPhi1(p), n2) * phi2(p);
-			double meanGradPhi2_scal_jumpPhi1 = weight2 * k2 * InnerProduct(gradPhi2(p), n1) * phi1(p);
-			return meanGradPhi1_scal_jumpPhi2 + meanGradPhi2_scal_jumpPhi1;
-		};
-
-		int nQuadPoints = p_phi1->GetDegree() + p_phi2->GetDegree() + 1;
-		return -Utils::Integral(nQuadPoints, functionToIntegrate, -1, 1);
-	}
-
-	double PenalizationTerm(Element<2>* element1, BasisFunction<2>* p_phi1, Element<2>* element2, BasisFunction<2>* p_phi2, double penalizationCoefficient, DiffusionPartition diffusionPartition)
-	{
-		auto n1 = element1->OuterNormalVector(this);
-		auto n2 = element2->OuterNormalVector(this);
-
-		auto phi1 = element1->EvalPhiOnFace(this, p_phi1);
-		auto phi2 = element2->EvalPhiOnFace(this, p_phi2);
-
-		std::function<double(double)> functionToIntegrate = [phi1, phi2, n1, n2](double s) {
-			RefPoint p(s);
-			return InnerProduct(n1, n2) * phi1(p) * phi2(p);
-		};
-
-		int nQuadPoints = p_phi1->GetDegree() + p_phi2->GetDegree() + 2;
-		double h = this->Width;
-		double integralJump1ScalarJump2 = h / 2 * Utils::Integral(nQuadPoints, functionToIntegrate, -1, 1);
-
-		double diffusionDependantCoefficient = element1->DiffusionCoefficient(diffusionPartition);
-		if (!this->IsDomainBoundary)
-		{
-			double k1 = this->Element1->DiffusionCoefficient(diffusionPartition);
-			double k2 = this->Element2->DiffusionCoefficient(diffusionPartition);
-			diffusionDependantCoefficient = 2 * k1*k2 / (k1 + k2);
-		}
-
-		return diffusionDependantCoefficient * penalizationCoefficient * integralJump1ScalarJump2;
-	}
+		CartesianFace(number, origin, length, element1, NULL, orientation)
+	{ }
 
 	friend ostream& operator<<(ostream& os, const IntervalFace& face)
 	{
 		os << face.Number << "\t(" << face.CartesianShape::Origin.X << ", " << face.CartesianShape::Origin.Y << ")\t" << (face.CartesianShape::Orientation == CartesianShapeOrientation::Horizontal ? "horizontal" : "vertical");
 		return os;
-	}
-
-	//----------------------------------------------------------------//
-	//                 Poisson_HHO_Face implementation                //
-	//----------------------------------------------------------------//
-
-	Eigen::MatrixXd ComputeAndReturnFaceMassMatrix(FunctionalBasis<1>* basis)
-	{
-		return CartesianShape::ComputeAndReturnFaceMassMatrix(basis);
-	}
-
-	double ComputeIntegral(function<double(RefPoint)> func, int numberOfDerivatives)
-	{
-		return CartesianShape::ComputeIntegral(func, numberOfDerivatives);
-	}
-	double ComputeIntegral(function<double(RefPoint)> func, int numberOfDerivatives, int polynomialDegree)
-	{
-		return CartesianShape::ComputeIntegral(func, numberOfDerivatives, polynomialDegree);
-	}
-
-private:
-	static double InnerProduct(vector<double> vector1, vector<double> vector2)
-	{
-		return vector1[0] * vector2[0] + vector1[1] * vector2[1];
 	}
 };

src/Mesh/PointFace.h
@@ -51,11 +51,21 @@ public:
 		return vector<RefPoint> {RefPoint(0)};
 	}
 
+	double ComputeIntegral(function<double(RefPoint)> func, int numberOfDerivatives, int polynomialDegree)
+	{
+		assert(false);
+	}
+
+	double ComputeIntegral(function<double(RefPoint)> func, int numberOfDerivatives)
+	{
+		assert(false);
+	}
+
 	//---------------------------------------------------------------//
 	//                 Poisson_DG_Face implementation                //
 	//---------------------------------------------------------------//
 
-	double CouplingTerm(Element<1>* element1, BasisFunction<1>* p_phi1, Element<1>* element2, BasisFunction<1>* p_phi2, DiffusionPartition diffusionPartition)
+	double CouplingTerm(Element<1>* element1, BasisFunction<1>* p_phi1, Element<1>* element2, BasisFunction<1>* p_phi2, DiffusionPartition diffusionPartition) override
 	{
 		IBasisFunction1D* phi1 = static_cast<IBasisFunction1D*>(p_phi1);
 		IBasisFunction1D* phi2 = static_cast<IBasisFunction1D*>(p_phi2);
@@ -81,7 +91,7 @@ public:
 		return weight1 * k1 * MeanDerivative(interval1, phi1) * Jump(interval2, phi2) + weight2 * k2 * MeanDerivative(interval2, phi2) * Jump(interval1, phi1);
 	}
 
-	double PenalizationTerm(Element<1>* element1, BasisFunction<1>* p_phi1, Element<1>* element2, BasisFunction<1>* p_phi2, double penalizationCoefficient, DiffusionPartition diffusionPartition)
+	double PenalizationTerm(Element<1>* element1, BasisFunction<1>* p_phi1, Element<1>* element2, BasisFunction<1>* p_phi2, double penalizationCoefficient, DiffusionPartition diffusionPartition) override
 	{
 		IBasisFunction1D* phi1 = static_cast<IBasisFunction1D*>(p_phi1);
 		IBasisFunction1D* phi2 = static_cast<IBasisFunction1D*>(p_phi2);

commit 5493d1733b4a9f4cddf353964b7b2334cfcbe6f1

Fix bug in DG introduced in the changeset cfab9aa (2019‑05‑15) --> missing return and sign problem

src/DG/Poisson_DG_Face.h
@@ -42,6 +42,7 @@ public:
 
 		int polynomialDegree = max(p_phi1->GetDegree() + p_phi2->GetDegree() - 1, 0);
 		double integralJump1ScalarJump2 = this->ComputeIntegral(functionToIntegrate, polynomialDegree);
+		return -integralJump1ScalarJump2;
 	}
 
 	virtual double PenalizationTerm(Element<Dim>* element1, BasisFunction<Dim>* p_phi1, Element<Dim>* element2, BasisFunction<Dim>* p_phi2, double penalizationCoefficient)