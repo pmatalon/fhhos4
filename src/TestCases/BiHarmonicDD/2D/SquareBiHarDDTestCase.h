b08af62d69ce8ee5cc7d90de835d50a51b8a906b
Pierre Matalon <pierre.matalon@gmail.com>
2022-01-18 17:25:11 +0100

    [NORMAL] Remove wrong test case from BiHarmonicDD

--- src/TestCases/BiHarmonicDD/2D/SquareBiHarDDTestCase.h
+++ src/TestCases/BiHarmonicDD/2D/SquareBiHarDDTestCase.h
@@ -4,69 +4,31 @@ using namespace std;
 
 class SquareBiHarDDTestCase : public BiHarmonicDDTestCase<2>
 {
-private:
-	ProblemArguments _pb;
 public:
 	SquareBiHarDDTestCase(ProblemArguments pb) :
-		BiHarmonicDDTestCase(),
-		_pb(pb)
+		BiHarmonicDDTestCase()
 	{
 		// Boundary conditions
 		this->DirichletBC = BoundaryConditions::HomogeneousDirichletEverywhere();
-		if (pb.BCCode.compare("dd") == 0)
-			this->LaplacianDirichletBC = new BoundaryConditions(BoundaryConditions::HomogeneousDirichletEverywhere());
-		else if (pb.BCCode.compare("dn") == 0)
-			this->NeumannBC = new BoundaryConditions(BoundaryConditions::HomogeneousNeumannEverywhere());
-		else
-			Utils::FatalError("The requested boundary conditions (-bc " + pb.BCCode + ") are not defined for this test case.");
+		this->LaplacianDirichletBC = new BoundaryConditions(BoundaryConditions::HomogeneousDirichletEverywhere());
 
 		// Source function
-		if (pb.SourceCode.compare("") == 0)
-		{
-			if (pb.BCCode.compare("dd") == 0)
-			{
-				pb.SourceCode = "sine";
-				_pb.SourceCode = "sine";
-			}
-			else
-			{
-				pb.SourceCode = "poly";
-				_pb.SourceCode = "poly";
-			}
-		}
-		if (pb.SourceCode.compare("sine") == 0)
-			this->SourceFunction = this->SineSource2D;
-		else if (pb.SourceCode.compare("poly") == 0)
-			this->SourceFunction = this->PolySource2D;
-		else
-			Utils::FatalError("Unmanaged source code");
+		this->SourceFunction = this->SineSource2D;
 
 		// Exact solution
-		if (pb.GeoCode.compare("square") == 0 && pb.BCCode.compare("dd") == 0 && pb.SourceCode.compare("sine") == 0)
+		if (pb.GeoCode.compare("square") == 0)
 		{
 			this->MinusLaplacianOfSolution = this->SineMinusLaplacianOfSolution2D;
 			this->ExactSolution = this->SineSolution2D;
 		}
-		else if (pb.GeoCode.compare("square") == 0 && pb.BCCode.compare("dn") == 0 && pb.SourceCode.compare("poly") == 0)
-			this->ExactSolution = this->PolySolution2D;
 	}
 
 	string Code() override
 	{
-		if (_pb.SourceCode.compare("sine") == 0)
-			return "sine";
-		else if (_pb.SourceCode.compare("poly") == 0)
-			return "poly";
-		else
-			return _pb.SourceCode;
+		return "";
 	}
 	string Description() override
 	{
-		if (_pb.SourceCode.compare("sine") == 0)
-			return "sine solution";
-		else if (_pb.SourceCode.compare("poly") == 0)
-			return "polynomial solution";
-		else
-			return _pb.SourceCode;
+		return "";
 	}
 };
--- src/TestCases/BiHarmonicDD/BiHarmonicDDTestCase.h
+++ src/TestCases/BiHarmonicDD/BiHarmonicDDTestCase.h
@@ -9,7 +9,6 @@ class BiHarmonicDDTestCase : public TestCase<Dim>
 public:
 	DomFunction SourceFunction = nullptr;
 	BoundaryConditions DirichletBC;
-	BoundaryConditions* NeumannBC = nullptr;
 	BoundaryConditions* LaplacianDirichletBC = nullptr;
 
 	DomFunction MinusLaplacianOfSolution = nullptr;
@@ -22,11 +21,7 @@ public:
 		cout << "Problem: BiHarmonicDD " << Dim << "D" << endl;
 		//cout << "    Geometry           : " << this->_mesh->GeometryDescription() << endl;
 		cout << "    Test case          : " << this->Description() << endl;
-		cout << "    Boundary conditions: Dirichlet + ";
-		if (NeumannBC)
-			cout << "Neumann";
-		else
-			cout << "Dirichlet on laplacian";
+		cout << "    Boundary conditions: Dirichlet + Dirichlet on laplacian";
 		cout << endl;
 	}
 
@@ -54,20 +49,6 @@ protected:
 		double x = p.X;
 		double y = p.Y;
 		return sin(4 * M_PI * x) * sin(4 * M_PI * y);
-	}
-
-	// With mixed Dirichlet/Neumann B.C.
-	static double PolySource2D(const DomPoint& p)
-	{
-		double x = p.X;
-		double y = p.Y;
-		return 56400 * (1 - 10*x + 15*x*x) * pow(1 - y, 2) * pow(y, 4) + 18800 * x*x * (6 - 20 * x + 15 * x*x) * y*y* (6 - 20*y + 15*y*y) + 56400*pow(1-x, 2) * pow(x, 4) * (1 - 10*y + 15*y*y);
-	}
-	static double PolySolution2D(const DomPoint& p)
-	{
-		double x = p.X;
-		double y = p.Y;
-		return 2350*pow(x, 4)*pow(x-1, 2)*pow(y, 4)*pow(y-1, 2);
 	}
 
 public:
