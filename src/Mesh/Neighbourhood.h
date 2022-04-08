df7dcda643b3cda267433a335d74758c8d0b8534
Pierre Matalon <pierre.matalon@gmail.com>
2022-04-08 15:57:38 +0200

    [NORMAL] Neighbourhood by vertices

--- src/Mesh/Neighbourhood.h
+++ src/Mesh/Neighbourhood.h
@@ -10,18 +10,20 @@ public:
 	vector<Face<Dim>*> BoundaryFaces;
 	vector<Face<Dim>*> InteriorFaces;
 
-	// Builds the neighbourhood of an element with respect to shared faces.
+	// Builds the neighbourhood of an element with respect to shared vertices.
 	// If depth = 0, just the element e. If depth = 1, e + all its neighbours.
 	// If depth = 2, + all its neighbours' neighbours. Etc.
 	Neighbourhood(Element<Dim>* e, int depth = 1)
 	{
 		set<Element<Dim>*> patch;
+		set<MeshVertex<Dim>*> boundaryVertices;
 		set<Face<Dim>*> boundaryFaces;
 		set<Face<Dim>*> interiorFaces;
 
-		patch.insert(e);
-
-		AddNeighboursOf(e, depth, patch, boundaryFaces, interiorFaces);
+		AddToPatch(e, patch, boundaryVertices, boundaryFaces, interiorFaces);
+		
+		for (int i = 0; i < depth; i++)
+			AddOneLayer(patch, boundaryVertices, boundaryFaces, interiorFaces);
 
 		Elements = vector<Element<Dim>*>(patch.begin(), patch.end());
 		BoundaryFaces = vector<Face<Dim>*>(boundaryFaces.begin(), boundaryFaces.end());
@@ -29,42 +31,59 @@ public:
 	}
 
 private:
-	void AddNeighboursOf(Element<Dim>* e, int depth, set<Element<Dim>*>& patch, set<Face<Dim>*>& boundaryFaces, set<Face<Dim>*>& interiorFaces)
+	void AddToPatch(Element<Dim>* e, set<Element<Dim>*>& patch, set<MeshVertex<Dim>*>& boundaryVertices, set<Face<Dim>*>& boundaryFaces, set<Face<Dim>*>& interiorFaces)
 	{
-		if (depth == 0)
-		{
-			// Add no neighbour.
-			// All the faces that are not interior to the patch is a boundary of the patch
-			for (Face<Dim>* f : e->Faces)
-			{
-				if (interiorFaces.find(f) == interiorFaces.end())
-					boundaryFaces.insert(f);
-			}
-			return;
-		}
+		patch.insert(e);
 
+		// Manage faces
 		for (Face<Dim>* f : e->Faces)
 		{
-			if (f->IsDomainBoundary)
-			{
+			if (f->IsDomainBoundary || patch.find(f->GetNeighbour(e)) == patch.end())
 				boundaryFaces.insert(f);
-				continue;
-			}
-			
-			Element<Dim>* n = f->GetNeighbour(e);
-			
-			interiorFaces.insert(f);
-			auto it = boundaryFaces.find(f);
-			if (it != boundaryFaces.end())
-				boundaryFaces.erase(it);
-
-			// If neighbour not already included
-			if (patch.find(n) == patch.end())
+			else
 			{
-				// Add neighbour
-				patch.insert(n);
-				// Add its own neighbours
-				AddNeighboursOf(n, depth - 1, patch, boundaryFaces, interiorFaces);
+				interiorFaces.insert(f);
+				auto it = boundaryFaces.find(f);
+				if (it != boundaryFaces.end())
+					boundaryFaces.erase(it);
+			}
+		}
+
+		// Manage vertices
+		for (Vertex* v : e->Vertices())
+		{
+			MeshVertex<Dim>* mv = static_cast<MeshVertex<Dim>*>(v);
+			bool allInPatch = true;
+			for (Element<Dim>* ve : mv->Elements)
+			{
+				if (ve == e)
+					continue;
+				if (patch.find(ve) == patch.end())
+				{
+					allInPatch = false;
+					break;
+				}
+			}
+			if (allInPatch)
+			{
+				auto it = boundaryVertices.find(mv);
+				if (it != boundaryVertices.end())
+					boundaryVertices.erase(it);
+			}
+			else
+				boundaryVertices.insert(mv);
+		}
+	}
+
+	void AddOneLayer(set<Element<Dim>*>& patch, set<MeshVertex<Dim>*>& boundaryVertices, set<Face<Dim>*>& boundaryFaces, set<Face<Dim>*>& interiorFaces)
+	{
+		vector<MeshVertex<Dim>*> currentBoundaryVertices(boundaryVertices.begin(), boundaryVertices.end());
+		for (MeshVertex<Dim>* v : currentBoundaryVertices)
+		{
+			for (Element<Dim>* e : v->Elements)
+			{
+				if (patch.find(e) == patch.end())
+					AddToPatch(e, patch, boundaryVertices, boundaryFaces, interiorFaces);
 			}
 		}
 	}
